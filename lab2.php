4e338c96c14a0c3efa091c0228f8466c03a4917e
Bishal Sudarshan <64673905+b1sh4l@users.noreply.github.com>
2022-06-06 10:48:06 +0600

    Add files via upload
    Lab2

---
+++ lab2.php
@@ -0,0 +1,44 @@
+<?php
+
+$x = 6;
+$y = 2;
+//echo "Hello World".$x."\n";
+
+$x = array(23,54,66,90);
+foreach($x as $a)
+{
+	echo $a."<br>";
+}
+
+
+/*$i =1;
+while($i<=10)
+{
+	echo $i."<br>";
+	$i++;
+}
+
+for($j=11; $j<=20; $j++)
+{
+	echo $j."<br>";
+}
+
+$colors =  array("RED", 420, 69.2);
+print_r ($colors);
+echo "<br>";
+var_dump ($colors);
+$colors[3] = "Baymax"; 
+echo "<br>";
+var_dump ($colors);
+echo "<br>".$colors[0];
+
+echo "x : ".$x."<br>";
+echo "y : ".$y."<br>";
+echo "The summation of x & y : ".($x+$y)."<br>";
+echo "The subtraction of x & y : ".($x-$y)."<br>";
+echo "The multiplication of x & y : ".($x*$y."<br>");
+echo "The division of x & y : ".($x/$y)."<br>";
+
+$id = "20-42915-1";
+echo "\n My Id: ".$id;*/
+?>
