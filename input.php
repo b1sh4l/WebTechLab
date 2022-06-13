5beecb854d9106ace72a9d446f7da32d95837967
Bishal Sudarshan <64673905+b1sh4l@users.noreply.github.com>
2022-06-13 10:46:06 +0600

    Add files via upload

---
+++ input.php
@@ -0,0 +1,7 @@
+<form action="function2.php" method="post">
+Username: <input type="text" id="uname" name="uname"> 
+<br> <br>
+Password: <input type="password" id="pass" name="pass">
+<br> <br>
+<input type="submit" value="Log in" name="btnLogin">
+</form>
